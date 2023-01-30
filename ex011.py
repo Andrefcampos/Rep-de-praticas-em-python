560a79aef1edbee6f0c43e12fbbe479c37205475
André Campos <106924044+Andrefcampos@users.noreply.github.com>
2023-01-30 00:18:24 -0300

    Add files via upload

---
+++ ex011.py
@@ -0,0 +1,32 @@
+'''Programa que lê de 4 pessoas:
+-nome
+- idade
+- sexo
+mostrar:
+- média de idade
+- idade do homem mais velho
+- quantas mulheres tem menos de 20 anos.
+'''
+soma_idade = 0
+media_idade = 0
+maior_idade_homem = 0
+nome_mais_velho = ''
+soma_mulher = 0
+for i in range (1, 5):
+    print('----- {}ª PESSOA -----'.format(i))
+    nome = str(input('Nome: '))
+    idade = int(input('Idade: '))
+    sexo = str(input('Sexo [M/F]: ')).strip()
+    soma_idade += idade
+    if i == 1 and sexo in 'Mm':
+        maior_idade_homem = idade
+        nome_mais_velho = nome
+    if sexo in 'Mm' and idade > maior_idade_homem:
+        maior_idade_homem = idade
+        nome_mais_velho = nome
+    if sexo in 'Ff' and idade < 20:
+        soma_mulher += 1
+media_idade = soma_idade / 4
+print('A média de idade do grupo é de {:.1} anos.'.format(media_idade))
+print('A idade do homem mais velho é {} anos e ele se chama {}.'.format(maior_idade_homem, nome_mais_velho.title()))
+print('Ao todo são {} mulheres com menos de 20 anos.'.format(soma_mulher))
