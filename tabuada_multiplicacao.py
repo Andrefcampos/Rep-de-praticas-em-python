d1a026ba07155a8fa7796a5b3f64bdb221f03f17
André Campos <106924044+Andrefcampos@users.noreply.github.com>
2023-02-02 12:13:20 -0300

    Update tabuada_multiplicacao.py

--- tabuada_multiplicacao.py
+++ tabuada_multiplicacao.py
@@ -1,14 +1,10 @@
 '''Programa que mostra Tabuada de vários números de cada vez:
 - parar quando informar número negativo'''
-num = total = 1
-seguir = ''
 while True:
     num = int(input('Qual valor você quer calcular? '))
     if num > 0:
         for i in range (0, 10):
-            i += 1
-            total = num * i
-            print(f'{num} x {i} = {total:2}')
+            print(f'{num} x {i} = {num * i}')
     print('--' * 20)
     seguir = str(input('>>> Você quer continuar? [S/N] ')).strip().upper()[0]
     print('--' * 20)
