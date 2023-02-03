3cc597381a73eb3ece023c5aa8cfac75a2646fbb
André Campos <106924044+Andrefcampos@users.noreply.github.com>
2023-02-03 00:30:04 -0300

    Add files via upload

---
+++ ex006a.py
@@ -0,0 +1,15 @@
+print('==' * 20)
+print('{:-^40}'.format('BANCO MALCON X'))
+print('==' * 20)
+valor_saque = int(input('Valor do saque: R$ '))
+cedulas = [50, 20, 10, 1]
+for i in cedulas:
+    total_cedulas = valor_saque // i
+    valor_saque %= i
+    if total_cedulas > 0:
+        print(f'>> foi utilizado {total_cedulas} cédulas de R$ {i:.2f}')
+print('Para efetuar o seu saque.')
+print('==' * 20)
+print(' >>> O BANCO MALCON X agradece a sua preferência.')
+print('Adoramos atender você. Volte sempre!')
+print('==' * 20)
