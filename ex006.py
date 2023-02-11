0de314308fadce155b380cdec28a17ea258e275a
André Campos <106924044+Andrefcampos@users.noreply.github.com>
2023-02-11 20:19:04 -0300

    Add files via upload

---
+++ ex006.py
@@ -0,0 +1,5 @@
+'''Programa que o usuario digite uma expressão
+ qualquer que use parenteses.
+ - o aplicativo deverá analisar a expressão passada esta
+ com os parenteses abertos e fechados na ordem correta.'''
+exp =
