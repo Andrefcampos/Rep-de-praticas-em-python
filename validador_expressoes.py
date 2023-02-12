cbaddfe6bd5c598cd0937573645437fd23c300a9
André Campos <106924044+Andrefcampos@users.noreply.github.com>
2023-02-11 23:34:01 -0300

    Update validador_expressoes.py

--- validador_expressoes.py
+++ validador_expressoes.py
@@ -2,4 +2,18 @@
  qualquer que use parenteses.
  - o aplicativo deverá analisar a expressão passada esta
  com os parenteses abertos e fechados na ordem correta.'''
-exp =
+expressao = str(input('Digite uma epressão matamática: '))
+lista = []
+for i in expressao:
+    if i == '(':
+        lista.append('(')
+    elif i == ')':
+        if len(lista) > 0:
+            lista.pop()
+        else:
+            lista.append(')')
+            break
+if len(lista) == 0:
+    print('Sua expressão está válida.')
+else:
+    print('Sua expressão está inválida.')
