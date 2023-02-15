9b3ea31d5aac6cdfaae50e9162b1805174fd0678
André Campos <106924044+Andrefcampos@users.noreply.github.com>
2023-02-14 22:07:37 -0300

    Add files via upload

---
+++ ex009.py
@@ -0,0 +1,15 @@
+'''Programa que cria uma matriz 3x3
+- preencher com os valores lidos pelo teclado
+- mostrar a matriz na tela com a formação correta'''
+list = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+print('=' * 40)
+print(f'{"MATRIZ 3X3":^40}')
+print('=' * 40)
+for i in range (0, 3):
+    for c in range(0, 3):
+        list[i][c] = int(input(f'Digite um valor [{i}, {c}]: '))
+print('=' * 40)
+for i in range (0, 3):
+    for c in range(0, 3):
+        print(f'[{list[i][c]:^5}]', end='')
+    print()
