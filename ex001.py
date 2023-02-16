bcaf45aeae7a62fa608abe00620fe60455775d35
André Campos <106924044+Andrefcampos@users.noreply.github.com>
2023-02-15 23:31:25 -0300

    Add files via upload

---
+++ ex001.py
@@ -0,0 +1,14 @@
+'''Programa que leia o nome e a média de um aluno
+- guardar também a situação em um dicionário
+- no final mostrar o conteúdo da estrutura na tela.'''
+aluno = {}
+aluno['nome'] = str(input('Digite o nome do aluno: '))
+aluno['média'] = float(input('Digite a média: '))
+if aluno['média'] >= 7:
+    aluno['situação'] = 'Aprovado'
+elif 4 < aluno['média'] < 7:
+    aluno['situação'] = 'Recuperação'
+else:
+    aluno['situação'] = 'Reprovado'
+for k, v in aluno.items():
+    print(f'>>> {k} é igual a {v}.')
