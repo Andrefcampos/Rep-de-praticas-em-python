b023d2f6ec08ca9b4f70b6d19b0988c6cb735cbe
André Campos <106924044+Andrefcampos@users.noreply.github.com>
2023-02-17 23:40:43 -0300

    Add files via upload

---
+++ ex006.py
@@ -0,0 +1,8 @@
+'''Programa que aprimore o desafio 93
+- funcione com vários jogadores
+- incluindo um sistema de visualização detalhada do aproveitamento de cada jogador'''
+
+jogadores = {}
+jogadores['nome'] = str(input('Nome do jogador: '))
+total = int(input(''))
+gol = []
