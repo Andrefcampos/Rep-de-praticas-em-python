016a83394d2cb31907fb93d718bef818819bd1d7
André Campos <106924044+Andrefcampos@users.noreply.github.com>
2023-02-17 23:37:12 -0300

    Update ex005.py

--- ex005.py
+++ ex005.py
@@ -4,16 +4,42 @@
 - a média de idade do grupo
 - uma lista com todas as mulheres
 - uma lista com todas as pessoas com idade acima da média.'''
-dados = {}
-mulheres = []
-maior_media = []
-media = 0
-total_idade = 0
+dados = []
+pessoa = {}
+soma = media = 0
 while True:
-    nome = str(input('Nome: ')).index()
-    sexo = str(input('Sexo: [F/M] ')).strip().upper()[0]
-    idade = int(input('Idade: '))
-    exit = str(input('Deseja continuar? [S/N] ')).strip().upper()[0]
+    pessoa.clear()
+    pessoa['nome'] = str(input('Nome: ')).title()
+    while True:
+        pessoa['sexo'] = str(input('Sexo: ')).upper()[0]
+        if pessoa['sexo'] in 'MF':
+            break
+        print('Valor inválido, digite "F" ou "M"  para prosseguir.')
+    pessoa['idade'] = int(input('Idade: '))
+    soma += pessoa['idade']
+    dados.append(pessoa.copy())
+    while True:
+        exit = str(input('Deseja continuar? [S/N] ')).upper()[0]
+        if exit in 'SN':
+            break
+        print('Valor inválido digite "S" ou "N".')
     if exit == 'N':
         break
+print('='*30)
 print(dados)
+print(f'A - Ao todo temos {len(dados)} pessoas cadastradas.')
+media = soma / len(dados)
+print(f'B - A média de idade das pessoas cadastradas é {media:.2f} anos')
+print(f'C - As mulheres cadastradas foram: ', end='')
+for p in dados:
+    if p['sexo'] in 'F':
+        print(f'{p["nome"]} ', end='')
+print()
+print('D - Lista de pessoas com idadee acima da média: ', end='')
+for p in dados:
+    if p['idade'] > media:
+        print('    ')
+        for k, v in p.items():
+            print(f'{k} = {v};', end='')
+            print()
+print('>>>>>>> FIM <<<<<<<')
