d74b0cca09eec39fadb3186876dffccc3a5c95a3
André Campos <106924044+Andrefcampos@users.noreply.github.com>
2023-02-17 23:59:38 -0300

    Update ex006.py

--- ex006.py
+++ ex006.py
@@ -1,8 +1,24 @@
 '''Programa que aprimore o desafio 93
 - funcione com vários jogadores
 - incluindo um sistema de visualização detalhada do aproveitamento de cada jogador'''
-
-jogadores = {}
-jogadores['nome'] = str(input('Nome do jogador: '))
-total = int(input(''))
-gol = []
+ficha_jogadores = []
+gols = []
+jogador = {}
+while True:
+    jogador.clear()
+    jogador['nome'] = str(input('Nome do jogador: ')).title()
+    jogador['partidas'] = int(input(f'Quantas partidas o jogador {jogador["nome"]} atuou? '))
+    for g in range(0, jogador['partidas']):
+        gols.append(int(input(f'O jogador {jogador["nome"]} marcou quantos gols na {g+1}ª partida: ')))
+    jogador['gols'] = gols[:]
+    gols.clear()
+    ficha_jogadores.append(jogador.copy())
+    while True:
+        exit = str(input('Deseja continuar? [S/N] ')).upper()[0]
+        if exit in 'SN':
+            break
+        print('Valor inválido, digite "S" ou "N".')
+    if exit == 'N':
+        break
+print(ficha_jogadores)
+print('='*40)
