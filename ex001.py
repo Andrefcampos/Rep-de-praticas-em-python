d3de7892bad098b4bf92a91873c805e1f0e4b57b
André Campos <106924044+Andrefcampos@users.noreply.github.com>
2023-02-20 22:53:11 -0300

    Update ex001.py

--- ex001.py
+++ ex001.py
@@ -1,1 +1,17 @@
-''''''
+'''Programa que tenha uma função chamada área que mostre:
+- terreno retangular (largura e comprimento
+- mostrar a área do terreno.'''
+def area():
+    largura = int(input('Largura: '))
+    comprimento = int(input('Comprimento: '))
+    tot_area = largura * comprimento
+    print(f'> A largura do terreno é de {largura}m')
+    print(f'>> O comprimento do terreno é de {comprimento}m')
+    print('-'*40)
+    print(f'>>> A área do terreno é de {tot_area}m²')
+
+
+print('='*40)
+print(f'{"MEDIÇÃO TERRENO":^40}')
+print('='*40)
+area()
