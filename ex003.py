5627f3df343fae7d2144ee2d40a98ea4e9bc1f53
André Campos <106924044+Andrefcampos@users.noreply.github.com>
2023-02-21 23:36:50 -0300

    Update ex003.py

---
+++ ex003.py
@@ -0,0 +1,7 @@
+'''Programa que tenha uma função contador():
+- que receba três parâmetros: inicio, fim e passo;
+- relize a contagem;
+- programa tem que realizar três contagens através da função
+- a. de 1 a 10, de 1 em 1
+- b. de 10 a 0, de 2 em 2
+- c. uma contagem personalizada.'''
