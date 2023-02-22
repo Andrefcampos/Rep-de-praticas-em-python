536d3f158f8ee48d56cb953fcdf3711650647ed9
André Campos <106924044+Andrefcampos@users.noreply.github.com>
2023-02-21 23:35:19 -0300

    Update ex004.py

--- ex004.py
+++ ex004.py
@@ -1,2 +1,21 @@
 '''Programa que tenha uma função chamada maior() que receba vários parametros com valores inteiros:
 - analisar todos os valores e dizer qual deles é o maior.'''
+def maior():
+    maior = 0
+    for i in range(0, numeros):
+        if i == 0:
+            maior = i = 0
+            print(f'O maior valor foi {maior}.')
+        elif maior > i:
+            maior = i
+            print(f'O maior valor foi {maior}.')
+
+
+numeros = []
+while True:
+    numeros.append(int(input('Digite valores: ')))
+    exit = str(input('Deseja continuar? [S/N] ')).strip().upper()[0]
+    if exit == 'N':
+        print('Obrigado!')
+        break
+print(f'Os valores foram: {numeros} e o maior valor foi {maior()}')
