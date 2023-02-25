eaa3ef8ec651bdd7aeae529461a68eadb153b035
André Campos <106924044+Andrefcampos@users.noreply.github.com>
2023-02-24 22:42:26 -0300

    Add files via upload

---
+++ ex006.py
@@ -0,0 +1,7 @@
+'''Programa que tenha:
+- a função chamada voto()
+- recber como parametro o ano de nascimento de uma pessoa
+- retornando um valor literal indicando se a pessoa tem voto
+    - negado
+    - opcional
+    - obrigatório'''
