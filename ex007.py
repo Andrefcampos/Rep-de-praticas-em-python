f518d146aee06d2d7280570b593d4c7609bd12e8
André Campos <106924044+Andrefcampos@users.noreply.github.com>
2023-02-25 16:28:33 -0300

    Update ex007.py

--- ex007.py
+++ ex007.py
@@ -1,21 +1,30 @@
 '''Programa que tenha:
-- a função chamada voto()
-- receber como parametro o ano de nascimento de uma pessoa
-- retornando um valor literal indicando se a pessoa tem voto
-    - negado
-    - opcional
-    - obrigatório'''
-def voto(ano):
-    from datetime import date
-    hoje = date.today().year
-    idade = hoje - ano
-    if 16 <= idade <= 65:
-        if 18 <= idade <= 65:
-            return print('OBRIGATÓRIO')
-        else:
-            return print('OPCIONAL')
-    else:
-        return print('NEGADO')
-anoNasc = int(input('Ano de nascimento: '))
-print('>>> O seu voto é: ', end='')
-voto(anoNasc)
+- função chamada fatorial()
+- que tenha dois parametros
+    - número a calcular
+    - show que será o valor lógico (opcional)
+- indicando se será mostrado ou não na tela o processo de calculo fatorial'''
+def fatorial(n, show=False):
+    """
+    -> Calcula o Fatorial de um número.
+    :param n: O número a ser calculado.
+    :param show: (opcional) Mostrar ou não a operação.
+    :return: O valor do Fatorial de um número n.
+    """
+
+    f = 1
+    for i in range(n, 0, -1):
+        if show:
+            print(i, end='')
+            if i > 1:
+                print(' x ', end='')
+            else:
+                print(' = ', end='')
+        f *= i
+    return f
+
+
+#Programa principal
+num = int(input('Digite um número: '))
+print(fatorial(num, True))
+help(fatorial)
