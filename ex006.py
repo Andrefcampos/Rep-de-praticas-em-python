d2a4f5c6eba145057bec0dad11f73f5e6921967a
André Campos <106924044+Andrefcampos@users.noreply.github.com>
2023-02-25 16:29:18 -0300

    Update ex006.py

--- ex006.py
+++ ex006.py
@@ -1,7 +1,21 @@
 '''Programa que tenha:
 - a função chamada voto()
-- recber como parametro o ano de nascimento de uma pessoa
+- receber como parametro o ano de nascimento de uma pessoa
 - retornando um valor literal indicando se a pessoa tem voto
     - negado
     - opcional
     - obrigatório'''
+def voto(ano):
+    from datetime import date
+    hoje = date.today().year
+    idade = hoje - ano
+    if 16 <= idade <= 65:
+        if 18 <= idade <= 65:
+            return print('OBRIGATÓRIO')
+        else:
+            return print('OPCIONAL')
+    else:
+        return print('NEGADO')
+anoNasc = int(input('Ano de nascimento: '))
+print('>>> O seu voto é: ', end='')
+voto(anoNasc)
