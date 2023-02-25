baafcfcc84e4b8bddc16ab8f30c92bd5622829ee
André Campos <106924044+Andrefcampos@users.noreply.github.com>
2023-02-24 22:42:39 -0300

    Add files via upload

---
+++ ex008.py
@@ -0,0 +1,6 @@
+''' Programa que contenha função:
+- ficha() que recebe dois parametros opcionais
+    - nome
+    - gols
+- O programa deverá ser capaz de mostrar a ficha do jogador
+mesmo que algum dado não tenha sido informado corretamente.'''
