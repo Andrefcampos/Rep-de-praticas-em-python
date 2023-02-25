69763a2430cb129dc248c738d56116ed99103e67
André Campos <106924044+Andrefcampos@users.noreply.github.com>
2023-02-25 16:45:50 -0300

    Update ex008.py

--- ex008.py
+++ ex008.py
@@ -4,3 +4,16 @@
     - gols
 - O programa deverá ser capaz de mostrar a ficha do jogador
 mesmo que algum dado não tenha sido informado corretamente.'''
+def ficha(nome='Desconhecido', gols=0):
+    print(f'O jogador {nome} fez {gols} gol(s) no campeonato.')
+
+
+n = str(input('Nome do jogador: '))
+try:
+    g = int(input(f'Quantos gols o jogdor {n} fez no campeonato? '))
+except:
+    g = 0
+if n.strip() == '':
+    ficha(gols=g)
+else:
+    ficha(n, g)
