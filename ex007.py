c2ed8ffce37edbb77609b80556e705c17f5aee5a
André Campos <106924044+Andrefcampos@users.noreply.github.com>
2023-02-24 22:39:17 -0300

    Update ex007.py

--- ex007.py
+++ ex007.py
@@ -1,1 +1,6 @@
-'''Programa com funções'''
+'''Programa que tenha:
+- função chamada fatorial()
+- que tenha dois parametros
+    - número a calcular
+    - show que será o valor lógico (opcional)
+- indicando se será mostrado ou não na tela o processo de calculo fatorial'''
