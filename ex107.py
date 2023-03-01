2f1ea265216d40edb4c9d4b990a816c5728b5a17
André Campos <106924044+Andrefcampos@users.noreply.github.com>
2023-02-28 23:23:46 -0300

    Add files via upload

---
+++ ex107.py
@@ -0,0 +1,7 @@
+'''Módulo chamado moedas.py que tenha as funções incorporadas:
+- aumentar()
+- diminuir()
+- dobro()
+- metade()
+ -> Criar programa que utilize essas funções importando esse módulo.'''
+def aumentar(num):
