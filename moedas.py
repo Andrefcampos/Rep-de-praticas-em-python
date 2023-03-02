06324ace25933c86d7e360529abacb9cc32b2d53
André Campos <106924044+Andrefcampos@users.noreply.github.com>
2023-03-01 23:27:18 -0300

    Update moedas.py

--- moedas.py
+++ moedas.py
@@ -1,23 +1,23 @@
-def dobro(preço=0):
+
+
+def dobro(preço=0, formato=False):
     res = preço * 2
-    return res
+    return res if not formato else moeda(res)
 
 
-def metade(preço=0):
+def metade(preço=0, formato=False):
     res = preço / 2
-    return res
+    return res if not formato else moeda(res)
 
 
-def aumentar(preço=0, taxa=0):
-    res = preço * (taxa / 100)
-    return res
+def aumentar(preço=0, taxa=0, formato=False):
+    res = preço + (preço * taxa / 100)
+    return res if not formato else moeda(res)
 
-
-def diminuir(preço=0, taxa=0):
+def diminuir(preço=0, taxa=0, formato=False):
     res = preço - (preço * taxa / 100)
-    return res
+    return res if not formato else moeda(res)
 
 
 def moeda(preço=0, moeda='R$'):
     return f'{moeda}{preço:.2f}'.replace('.', ',')
- 
