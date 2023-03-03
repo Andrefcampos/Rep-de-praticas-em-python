fe3d341ff9a549dfc487a85fcd0dfd6942667bee
André Campos <106924044+Andrefcampos@users.noreply.github.com>
2023-03-02 21:00:20 -0300

    Add files via upload

--- ex110.py
+++ ex110.py
@@ -1,2 +1,5 @@
-'''Adicione ao módulo moeda.py criado nos desafios anteriores uma função chamada resumo(),
-que mostre na tela algumas informações geradas pelas funções que já temos no módulo criado até hoje.'''
+'''Modifique as funções que foram criadas no desfio 107 para que elas aceitem um parametro a mais
+informando se o valor retornado por elas vai ser formatado pela função moeda() desenvolvida no desafio 108.'''
+import moedas
+p = float(input('Digite um valor: R$ '))
+moedas.resumo(p)
